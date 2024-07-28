58c187010044bf60cfcd7ad8b706f3d3be7b69c8
ElisabettaMirti <belmirti@gmail.com>
2024-07-28 19:47:35 +0200

    seeded

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,16 +2,25 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i=0; $i < 50; $i++) {
+            $newProject = new Project();
+            $newProject->title = $faker->word();
+            $newProject->prog_langs = $faker->realText();
+            $newProject->due_to = $faker->dateTime();
+            $newProject->url = $faker->imageUrl(360, 360, 'projects', true);
+            $newProject->save();
+        }
     }
 }
